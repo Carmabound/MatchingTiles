b58682db58b41335101c062a1038a5fb69d2ce0b
Carmabound <raygelbaygel@gmail.com>
2023-05-21 20:12:14 -0400

    just a blank page with a "chat", figure out how to install node

---
+++ client/src/client.js
@@ -0,0 +1,29 @@
+const log = (text) => {
+    const parent = document.querySelector('#events');
+    const el = document.createElement('li');
+    el.innerHTML = text;
+
+    parent.appendChild(el);
+    parent.scrollTop = parent.scrollHeight;
+};
+
+const onChatSubmitted = (e) => {
+    e.preventDefault();
+
+    const input = document.querySelector('#chat');
+    const text = input.value;
+    input.value = '';
+
+    log(text);
+};
+
+(() => {
+    log('welcome');
+
+    document
+        .querySelector('#chat-form')
+        .addEventListener('submit', onChatSubmitted);
+    /*document
+        .querySelector('#chat-form')
+        .addEventListener('keypress', onChatSubmitted)*/
+})();
---
+++ server/server.js
@@ -0,0 +1,1 @@
+console.log('server will be here');
